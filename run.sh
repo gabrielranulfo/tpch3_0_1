8332b9bfad83b586ed9bd7ad08b6e03c75ae9842
Gabriel Branquinho <gabrielranulfo14@gmail.com>
2025-09-22 22:57:14 -0300

    Configuração Padrão

--- run.sh
+++ run.sh
@@ -24,16 +24,16 @@ export STORAGE_FORMAT
 export WORK_DIR
 
 # Cria o ambiente virtual e instala dependências
-#./scripts/create_env.sh
+./scripts/create_env.sh
 
 # Ativa o ambiente virtual
 source .venv/bin/activate
 
 # Gera os .tbl
-#./scripts/gen_data.sh
+./scripts/gen_data.sh
 
 # Converte para CSV e Parquet usando Polars
-#python3 scripts/convert_tbl.py
+python3 scripts/convert_tbl.py
 
 python3 -m queries.lib_dask
 python3 -m queries.lib_pandas
