dd8b9ccd4d0d94d0dbae3e8699e572b1bacfca53
Gabriel Branquinho <gabrielranulfo14@gmail.com>
2025-10-14 04:18:48 -0300

    Docker Final

--- scripts/create_env.sh
+++ scripts/create_env.sh
@@ -25,8 +25,8 @@ source "$VENV_DIR/bin/activate"
 
 # Baixa e extrai Spark e OpenJDK
 
-wget -q wget https://dlcdn.apache.org/spark/spark-4.0.1/spark-4.0.1-bin-hadoop3.tgz
-wget -q https://download.java.net/openjdk/jdk17.0.0.1/ri/openjdk-17.0.0.1+2_linux-x64_bin.tar.gz
+#wget -q wget https://dlcdn.apache.org/spark/spark-4.0.1/spark-4.0.1-bin-hadoop3.tgz
+#wget -q https://download.java.net/openjdk/jdk17.0.0.1/ri/openjdk-17.0.0.1+2_linux-x64_bin.tar.gz
 
-tar xf spark-4.0.1-bin-hadoop3.tgz
-tar xf openjdk-17.0.0.1+2_linux-x64_bin.tar.gz
+#tar xf spark-4.0.1-bin-hadoop3.tgz
+#tar xf openjdk-17.0.0.1+2_linux-x64_bin.tar.gz
